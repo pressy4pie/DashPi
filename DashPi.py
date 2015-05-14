b5b67cf46093d3fc47f8400003334489dbad1993
Connor Rigby <pressy4pie@pressy4pie.com>
2015-05-14 15:46:11 -0700

    Update DashPi.py
    Removed the DashInfo class and changed how it is all set up. this is actually untested.

--- DashPi.py
+++ DashPi.py
@@ -4,25 +4,6 @@ import time
 import random
 import serial
 
-class dash :
-    def __init__ (self)
-	ser = serial.Serial('/dev/ttyAMA0', 115200)
-	ser.write(' ')
-
-    def get_speed(self):
-	# The function to get speed from the stuff. for now it will be spoofed to 65 mph.	
-	return 100
-
-    def get_rpm(self):
-	# Read the negitive side of the ignition coil to get rpm. 
-	# It is just pulses so it should just count them then do some math to get rotations per minute.
-	# For now we set it to a fixed number till i create the hardware and maybe get an arduino in the mix.
-	return 1500
-
-    def get_gas(self):
-	# The gas sender is just a resistor that i should be able to measure the resistance across. I don't know what the data will look like so thie is just a filler.
-	return 0
-
 class pyscope :
     screen = None;
     
@@ -75,9 +56,7 @@ pygame.display.Info().current_h)
 
 # This is where the main stuff starts...
 # Create an instance of the PyScope class
-# Initialize dashinfo
 scope = pyscope()
-dashinfo = dash()
 # Don't display cursor.
 pygame.mouse.set_visible(False)
 # Make a font
@@ -87,11 +66,6 @@ font = pygame.font.Font(None, 30)
 scope.screen.fill((0,0,0))
 pygame.display.update()
 
-
-# Images
-#speedo_bac = pygame.image.load('test_speedo.png').convert()
-#scope.screen.blit(speedo_bac,(0,0))
-
 # The test screen to be drawn on cuz i cant figure out how to make a surface this size..
 pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,240) ,1)
 pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,20), 1)
@@ -99,19 +73,30 @@ pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,20), 1)
 # Top Header thing
 scope.write("Welcome Connor",85,1)
 
+# Update the screen before starting the main loop
 pygame.display.update()
-ev = pygame.event.poll()
+
+# Start the serial stuff.
+# The arduino doesn't start reporting data untill it gets some data.
+# Any ol' data will do.
 ser = serial.Serial('/dev/ttyACM0', 115200)
 ser.write(str(chr(1))) # Send some data to get some data...
+
+ev = pygame.event.poll()
 while ev.type != pygame.QUIT:
 	# Do the main stuff.
-	print ser.readline()
-
-	speed = dashinfo.get_speed()
-	rpm = dashinfo.get_rpm()
-	gas = dashinfo.get_gas()
+	
+	# The serial data will be formatted as such: 
+	# 65.00 1500 50
+	# MPH   RPM  GAS%
+	line = ser.readline()
+	String[] splitLine = line.split()
+	speed = splitLine[0]
+	rpm = splitLine[1]
+	gas = splitLine[2]
+	
 	# Of course this will look better but for now it works.
-	# scope.write('Current Speed: {} MPH'.format(speed), 30, 45)
+	scope.write('Current Speed: {} MPH'.format(speed), 30, 45)
 	scope.write('Current RPMs: {} RPM'.format(rpm), 30, 115)
 	scope.write('Current Gas Meter: {}% '.format(gas), 30, 190) 
 
