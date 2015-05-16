90c0e35a5eaba00766eb89387f80105876e7c4bc
pressy4pie <pressy4pie@pressy4pie.com>
2015-05-16 11:20:46 -0700

    added DroidSans as a font and updated the main program It's mostly working i think.

--- DashPi.py
+++ DashPi.py
@@ -6,7 +6,7 @@ import serial
 
 class pyscope :
     screen = None;
-    
+
     def __init__(self):
         "Ininitializes a new pygame screen using the framebuffer"
         # Based on "Python GUI in Linux frame buffer"
@@ -14,7 +14,7 @@ class pyscope :
         disp_no = os.getenv("DISPLAY")
         if disp_no:
             print "I'm running under X display = {0}".format(disp_no)
-        
+
         # Check which frame buffer drivers are available
         # Start with fbcon since directfb hangs with composite output
         drivers = ['fbcon', 'directfb', 'svgalib']
@@ -30,11 +30,11 @@ class pyscope :
                 continue
             found = True
             break
-    
+
         if not found:
             raise Exception('No suitable video driver found!')
-        
-        size = (pygame.display.Info().current_w, 
+
+        size = (pygame.display.Info().current_w,
 pygame.display.Info().current_h)
         print "Full framebuffer size: %d x %d" % (size[0], size[1])
         self.screen = pygame.display.set_mode((size[0], size[1]),pygame.FULLSCREEN)
@@ -47,7 +47,7 @@ pygame.display.Info().current_h)
 
     def __del__(self):
         "Destructor to make sure pygame shuts down, etc."
-    
+
     def write(self, input_text, x, y):
 	font = pygame.font.Font(None, 30)
         text_surface = font.render(input_text, True, (255, 255, 255))  # White text
@@ -60,17 +60,20 @@ scope = pyscope()
 # Don't display cursor.
 pygame.mouse.set_visible(False)
 # Make a font
-font = pygame.font.Font(None, 30)
+font = pygame.font.Font('DroidSans.ttf', 30)
 
 # Fill/clear the screen & Update.
 scope.screen.fill((0,0,0))
 pygame.display.update()
 
 # The test screen to be drawn on cuz i cant figure out how to make a surface this size..
-pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,240) ,1)
-pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,20), 1)
+main_rect = pygame.draw.rect(scope.screen, (0,0,0), (0,0,320,240))
+main_rect_outline = pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,240),1)
+header_rect = pygame.draw.rect(scope.screen, (0,0,0), (0,0,320,20))
+header_rect = pygame.draw.rect(scope.screen, (255,255,255), (0,0,320,20),1)
 
 # Top Header thing
+# I'm sure ill display some actual information at some point.
 scope.write("Welcome Connor",85,1)
 
 # Update the screen before starting the main loop
@@ -85,28 +88,35 @@ ser.write(str(chr(1))) # Send some data to get some data...
 ev = pygame.event.poll()
 while ev.type != pygame.QUIT:
 	# Do the main stuff.
-	
-	# The serial data will be formatted as such: 
+
+	# The serial data will be formatted as such:
 	# 65.00 1500 50
 	# MPH   RPM  GAS%
-	line = ser.readline()
-	# print line
-	splitLine = line.split()
-	speed = splitLine[0]
-	rpm = splitLine[1]
-	gas = splitLine[2]
 
-	print speed
-	print rpm
-	print gas
+	# Get the line for serial, split it up and spit it with scope.write.
+	line = ser.readline() # This is the actual serial command
+	splitLine = line.split()
+	# print len(splitLine)
+
+	if len(splitLine) < 4: # Make sure we have the correct data here.
+		speed = splitLine[0]
+		rpm = splitLine[1]
+		gas = splitLine[2]
+
+
+	#Just for testing. This is to spammy. maybe ill add a verbose mode or debug mode or something.
+	#print speed
+	#print rpm
+	#print gas
 
 	# Of course this will look better but for now it works.
 	scope.write('Current Speed: {} MPH'.format(speed), 30, 45)
 	scope.write('Current RPMs: {} RPM'.format(rpm), 30, 115)
-	scope.write('Current Gas Meter: {}% '.format(gas), 30, 190) 
-
+	scope.write('Current Gas Meter: {}% '.format(gas), 30, 190)
+	# Update the display so we can see what's up.
 	pygame.display.update()
 
+	main_rect = pygame.draw.rect(scope.screen, (0,0,0), (0,20,320,220))
 
 
 #exit in 2 seconds
